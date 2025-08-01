e76f5b531a54774381c0bb88b45c06c255c8bd42
shrutikapoor08 <shruti.kapoor08@gmail.com>
2025-08-01 16:21:49 -0700

    Debounce

---
+++ debounce.js
@@ -0,0 +1,32 @@
+// Implement a Debounce Function
+
+// if function is being called when the timer is running, do not do anthing and reset the timer. 
+function debounce(func, wait) {
+  let timeout;
+    return function (){
+        const args = arguments;
+        const thisContext = this;
+
+        if(timeout) clearTimeout(timeout);
+        timeout = setTimeout( () => func.apply(thisContext, args), wait);
+
+    }
+}
+
+
+const log = (i) => console.log("Log!", i);
+const debouncedLog = debounce(log, 500);
+
+
+for (let i = 0; i < 1000; i++) {
+  (function (i) {
+    debouncedLog(i);
+  })(i);
+}
+
+
+for (let i = 0; i < 200; i++) {
+  (function (i) {
+    debouncedLog(i);
+  })(i);
+}
