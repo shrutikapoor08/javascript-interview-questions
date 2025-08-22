ef3e4f71726020a13c7d3700899bc8e5d0685bde
shrutikapoor08 <shruti.kapoor08@gmail.com>
2025-08-22 08:16:25 -0700

    full promise

--- Promise-Interview.js
+++ Promise-Interview.js
@@ -10,19 +10,35 @@ class MyPromise {
         const resolve = (val) => {
             if (this.state !== 'pending') return;
 
+             if (val instanceof MyPromise) {
+                    val.then(resolve, reject);
+                    return;
+                }
+            queueMicrotask(() => {
+               
             this.state = 'fulfilled';
             this.value = val;
             this.thenCallbacks.forEach(cb => cb(val));
+            this.thenCallbacks = [];
+            this.catchCallbacks = [];
+            });
 
 
         }
-        const reject = (val) => {
+        const reject = (err) => {
             if (this.state !== 'pending') return;
+              if (err instanceof MyPromise) {
+                    err.then(resolve, reject);
+                    return;
+                }
 
+                queueMicrotask(() => {
             this.state = 'rejected';
-            this.value = val;
-            this.catchCallbacks.forEach(cb => cb(val));
-
+            this.value = err;
+            this.catchCallbacks.forEach(cb => cb(err));
+            this.catchCallbacks = [];
+            this.thenCallbacks = [];
+                });
         }
 
         try {
@@ -33,17 +49,57 @@ class MyPromise {
     }
 
     then(thenCb, catchCb) {
-        if (this.state === 'pending') {
-            if (thenCb) this.thenCallbacks.push(thenCb);
-            if (catchCb) this.catchCallbacks.push(catchCb);
-        }
-        else if (this.state === 'fulfilled') {
-            this.thenCallbacks.forEach(cb => { cb(this.value); });
-        }
-        else if (this.state === 'rejected') {
-            this.catchCallbacks.forEach(cb => { cb(this.value); });
-        }
-        return this;
+        return new MyPromise((resolve, reject) => {
+
+            const onFulfilled = (val) => {
+                if (typeof thenCb !== 'function') {
+                    resolve(val);
+                    return;
+                }
+                try {
+                    const result = thenCb(val);
+                    if (result instanceof MyPromise) {
+                        result.then(resolve, reject);
+                    } else {
+                        resolve(result);
+                    }
+                } catch (error) {
+                    reject(error);
+                }
+            };
+
+            const onRejected = (err) => {
+                if (typeof catchCb !== 'function') {
+                    reject(err);
+                    return;
+                }
+                try {
+                    const result = catchCb(err);
+                    if (result instanceof MyPromise) {
+                        result.then(resolve, reject);
+                    } else {
+                        resolve(result);
+                    }
+                } catch (error) {
+                    reject(error);
+                }
+            };
+
+
+            if (this.state === 'pending') {
+                this.thenCallbacks.push(onFulfilled);
+                this.catchCallbacks.push(onRejected);
+            }
+            else if (this.state === 'fulfilled') {
+
+                queueMicrotask(() => onFulfilled(this.value));
+
+
+            }
+            else if (this.state === 'rejected') {
+                queueMicrotask(() => onRejected(this.value));
+            }
+        });
 
     }
 
@@ -69,14 +125,21 @@ const myPromise = new MyPromise((resolve, reject) => {
 myPromise
     .then((result) => {
         console.log(result);
+        return result + " - processed";
     })
     .then((result) => {
         console.log("Second then", result);
+        return result + " - more processing";
     })
     .catch((error) => {
         console.error("Error:", error);
     })
     .then((result) => {
         console.log("Third then after catch", result);
-    });
-
+    })
+    .then((result) => {
+        console.log("Fourth then", result);
+    })
+    .catch((error) => {
+        console.error("Second catch:", error);
+    })
