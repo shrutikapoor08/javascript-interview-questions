fb4572454b42c52fb3b22f5d58ec09e636b59dee
shrutikapoor08 <shruti.kapoor08@gmail.com>
2025-08-24 11:52:56 -0700

    starting point

--- Promise-Interview.js
+++ Promise-Interview.js
@@ -5,117 +5,17 @@ class MyPromise {
         this.value = undefined;
         this.thenCallbacks = [];
         this.catchCallbacks = [];
-
-
-        const resolve = (val) => {
-            if (this.state !== 'pending') return;
-
-             if (val instanceof MyPromise) {
-                    val.then(resolve, reject);
-                    return;
-                }
-            queueMicrotask(() => {
-               
-            this.state = 'fulfilled';
-            this.value = val;
-            this.thenCallbacks.forEach(cb => cb(val));
-            this.thenCallbacks = [];
-            this.catchCallbacks = [];
-            });
-
-
-        }
-        const reject = (err) => {
-            if (this.state !== 'pending') return;
-              if (err instanceof MyPromise) {
-                    err.then(resolve, reject);
-                    return;
-                }
-
-                queueMicrotask(() => {
-            this.state = 'rejected';
-            this.value = err;
-            this.catchCallbacks.forEach(cb => cb(err));
-            this.catchCallbacks = [];
-            this.thenCallbacks = [];
-                });
-        }
-
-        try {
-            cb(resolve, reject);
-        } catch (error) {
-            reject(error);
-        }
     }
 
-    then(thenCb, catchCb) {
-        return new MyPromise((resolve, reject) => {
-
-            const onFulfilled = (val) => {
-                if (typeof thenCb !== 'function') {
-                    resolve(val);
-                    return;
-                }
-                try {
-                    const result = thenCb(val);
-                    if (result instanceof MyPromise) {
-                        result.then(resolve, reject);
-                    } else {
-                        resolve(result);
-                    }
-                } catch (error) {
-                    reject(error);
-                }
-            };
-
-            const onRejected = (err) => {
-                if (typeof catchCb !== 'function') {
-                    reject(err);
-                    return;
-                }
-                try {
-                    const result = catchCb(err);
-                    if (result instanceof MyPromise) {
-                        result.then(resolve, reject);
-                    } else {
-                        resolve(result);
-                    }
-                } catch (error) {
-                    reject(error);
-                }
-            };
-
-
-            if (this.state === 'pending') {
-                this.thenCallbacks.push(onFulfilled);
-                this.catchCallbacks.push(onRejected);
-            }
-            else if (this.state === 'fulfilled') {
-
-                queueMicrotask(() => onFulfilled(this.value));
-
-
-            }
-            else if (this.state === 'rejected') {
-                queueMicrotask(() => onRejected(this.value));
-            }
-        });
+    then() {
 
     }
-
-    catch(catchCb) {
-        return this.then(undefined, catchCb);
+    catch() {  
+    }
+    finally() {
+        
     }
 
-    finally(cb) {
-return this.then( result => {
-    cb();
-    return result;
-}, error => {  
-    cb();
-    throw error;
-});
-    }
 }
 
 const myPromise = new MyPromise((resolve, reject) => {
